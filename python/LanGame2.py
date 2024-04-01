07ad605dd95498331370de72b2712de42dc89f83
ryaddaoud21 <m.daoud@esi-sba.dz>
2024-04-01 16:49:03 +0200

    Refactorisation de la méthode score pour simplifier la logique en utilisant des méthodes d'aide + Introduction de la prise en charge des langues pour les scores

--- python/LanGame2.py
+++ python/LanGame2.py
@@ -1,95 +1,54 @@
 # -*- coding: utf-8 -*-
 
 class TennisGame2:
-    def __init__(self, player1Name, player2Name):
+    SCORE_NAMES_EN = ["Love", "Fifteen", "Thirty", "Forty"]
+    SCORE_NAMES_FR = ["Zéro", "Quinze", "Trente", "Quarante"]
+
+    def __init__(self, player1Name, player2Name, language):
         self.player1Name = player1Name
         self.player2Name = player2Name
-        self.score_player1  = 0
-        self.p2points = 0
+        self.score_player1 = 0
+        self.score_player2 = 0
+        self.language = language
 
     def won_point(self, playerName):
-        if playerName == "player1":
-            self.P1Score()
-        elif playerName == "player2":
-            self.P2Score()
+        if playerName == self.player1Name:
+            self.score_player1 += 1
+        elif playerName == self.player2Name:
+            self.score_player2 += 1
         else:
             raise ValueError("Player name is not recognized")
-        
+
     def score(self):
-        result = ""
-        if (self.score_player1  == self.score_player2 and self.score_player1  < 3):
-            if (self.score_player1 ==0):
-                result = "Love"
-            if (self.score_player1 ==1):
-                result = "Fifteen"
-            if (self.score_player1 ==2):
-                result = "Thirty"
-            result += "-All"
-        if (self.score_player1 ==self.score_player2 and self.score_player1 >2):
-            result = "Deuce"
+        if self.is_deuce():
+            return "Deuce" if self.language == 'EN' else "Égalité"
+        if self.is_advantage():
+            player_advantage = self.advantage_player()
+            return f"Advantage {player_advantage}" if self.language == 'EN' else f"Avantage {player_advantage}"
+        if self.is_win():
+            player_winner = self.winning_player()
+            return f"Win for {player_winner}" if self.language == 'EN' else f"Victoire pour {player_winner}"
+        return self.current_score()
 
-        P1res = ""
-        P2res = ""
-        if (self.score_player1  > 0 and self.score_player2==0):
-            if (self.score_player1 ==1):
-                P1res = "Fifteen"
-            if (self.score_player1 ==2):
-                P1res = "Thirty"
-            if (self.score_player1 ==3):
-                P1res = "Forty"
+    def is_deuce(self):
+        return self.score_player1 >= 3 and self.score_player1 == self.score_player2
 
-            P2res = "Love"
-            result = P1res + "-" + P2res
-        if (self.score_player2 > 0 and self.score_player1 ==0):
-            if (self.score_player2==1):
-                P2res = "Fifteen"
-            if (self.score_player2==2):
-                P2res = "Thirty"
-            if (self.score_player2==3):
-                P2res = "Forty"
+    def is_advantage(self):
+        return abs(self.score_player1 - self.score_player2) == 1 and max(self.score_player1, self.score_player2) >= 4
 
-            P1res = "Love"
-            result = P1res + "-" + P2res
+    def is_win(self):
+        return abs(self.score_player1 - self.score_player2) >= 2 and max(self.score_player1, self.score_player2) >= 4
 
+    def advantage_player(self):
+        return self.player1Name if self.score_player1 > self.score_player2 else self.player2Name
 
-        if (self.score_player1 >self.score_player2 and self.score_player1  < 4):
-            if (self.score_player1 ==2):
-                P1res="Thirty"
-            if (self.score_player1 ==3):
-                P1res="Forty"
-            if (self.score_player2==1):
-                P2res="Fifteen"
-            if (self.score_player2==2):
-                P2res="Thirty"
-            result = P1res + "-" + P2res
-        if (self.score_player2>self.score_player1  and self.score_player2 < 4):
-            if (self.score_player2==2):
-                P2res="Thirty"
-            if (self.score_player2==3):
-                P2res="Forty"
-            if (self.score_player1 ==1):
-                P1res="Fifteen"
-            if (self.score_player1 ==2):
-                P1res="Thirty"
-            result = P1res + "-" + P2res
+    def winning_player(self):
+        return self.player1Name if self.score_player1 > self.score_player2 else self.player2Name
 
-        if (self.score_player1  > self.score_player2 and self.score_player2 >= 3):
-            result = "Advantage player1"
-
-        if (self.score_player2 > self.score_player1  and self.score_player1  >= 3):
-            result = "Advantage player2"
-
-        if (self.score_player1 >=4 and self.score_player2>=0 and (self.score_player1 -self.score_player2)>=2):
-            result = "Win for player1"
-        if (self.score_player2>=4 and self.score_player1 >=0 and (self.score_player2-self.score_player1 )>=2):
-            result = "Win for player2"
-        return result
-
-
-
-    def P1Score(self):
-        self.score_player1  +=1
-
-
-    def P2Score(self):
-        self.score_player2 +=1
+    def current_score(self):
+        score_names = self.SCORE_NAMES_EN if self.language == 'EN' else self.SCORE_NAMES_FR
+        score_p1 = score_names[min(self.score_player1, 3)]
+        score_p2 = score_names[min(self.score_player2, 3)]
+        if self.score_player1 == self.score_player2:
+            return f"{score_p1}-All" if self.language == 'EN' else f"{score_p1}-A" if self.score_player1 < 3 else "Deuce" if self.language == 'EN' else "Égalité"
+        return f"{score_p1}-{score_p2}"
